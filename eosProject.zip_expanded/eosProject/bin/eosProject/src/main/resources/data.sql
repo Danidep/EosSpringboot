457ceea59adcdae5766e8af12a1780a40a481688
Daniela De Pascali <danidep@outlook.it>
2020-11-06 16:33:46 +0100

    rif

--- eosProject.zip_expanded/eosProject/bin/eosProject/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO USER(mail, password, name) VALUES()
--- eosProject.zip_expanded/eosProject/eosProject/src/main/java/it/eos/springuser/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.eos.springuser.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.eos.springuser.model.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, String>{
-
-}
